a35a01b02c925c443f7064e6b3068a817b6dd9bc
Manuele Vaccari <manuele.vaccari@gmail.com>
2018-02-11 00:49:39 +0100

    Update cloud

--- src/Controller/CloudController.php
+++ src/Controller/CloudController.php
@@ -17,13 +17,12 @@ class CloudController extends \Controller
         }
 
         if (!LOGGED_IN) {
-            return $this->redirectToRoute('app_account_login', array('redir' => $this->getRequest()->getUri()));
+            return $this->redirectToRoute('app_account_login', ['redir' => $this->getRequest()->getUri()]);
         }
 
         return $this->redirectToRoute('app_cloud_files');
     }
 
-    // TODO: Add possibility to publish a website by crating a "public_html" folder in the root
     public function filesAction()
     {
         $em = $this->getEntityManager();
@@ -32,14 +31,14 @@ class CloudController extends \Controller
             return $this->redirectToRoute('app_account_logout');
         }
         if (!LOGGED_IN) {
-            return $this->redirectToRoute('app_account_login', array('redir' => $this->getRequest()->getUri()));
+            return $this->redirectToRoute('app_account_login', ['redir' => $this->getRequest()->getUri()]);
         }
         /** @var \App\Account\Entity\User $currentUser */
         $currentUser = $em->find(User::class, USER_ID);
 
-        return $this->render('cloud/files.html.twig', array(
+        return $this->render('cloud/files.html.twig', [
             'current_user' => $currentUser,
-        ));
+        ]);
     }
 
     public function showRawFileAction()
@@ -50,12 +49,12 @@ class CloudController extends \Controller
             return $this->redirectToRoute('app_account_logout');
         }
         if (!LOGGED_IN) {
-            return $this->redirectToRoute('app_account_login', array('redir' => $this->getRequest()->getUri()));
+            return $this->redirectToRoute('app_account_login', ['redir' => $this->getRequest()->getUri()]);
         }
         /** @var \App\Account\Entity\User $currentUser */
         $currentUser = $em->find(User::class, USER_ID);
 
-        $extensionToMime = array(
+        $extensionToMime = [
             'pdf'  => 'application/pdf',
             'zip'  => 'application/zip',
             'gif'  => 'image/gif',
@@ -66,7 +65,7 @@ class CloudController extends \Controller
             'js'   => 'text/javascript',
             'txt'  => 'text/plain',
             'xml'  => 'text/xml',
-        );
+        ];
 
         $fileDir = $this->get('kernel')->getRootDir().'/data/cloud/storage/'.$currentUser->getId().'/'.$this->parameters['file'];
         $fileInfo = pathinfo($fileDir);
@@ -78,18 +77,6 @@ class CloudController extends \Controller
         return $response;
     }
 
-    public function shareAction()
-    {
-        if (is_null(AccountHelper::updateSession())) {
-            return $this->redirectToRoute('app_account_logout');
-        }
-        if (!LOGGED_IN) {
-            return $this->redirectToRoute('app_account_login', array('redir' => $this->getRequest()->getUri()));
-        }
-
-        return $this->render('cloud/share.html.twig');
-    }
-
     public function connectorAction()
     {
         /** @var \Kernel $kernel */
@@ -97,77 +84,14 @@ class CloudController extends \Controller
         $em = $this->getEntityManager();
 
         if (is_null(AccountHelper::updateSession()) || !LOGGED_IN) {
-            return $this->json(array());
+            return $this->json([]);
         }
         /** @var \App\Account\Entity\User $currentUser */
         $currentUser = $em->find(User::class, USER_ID);
 
-        $functionsFile = $kernel->getRootDir().'/src/App/Cloud/functions.php';
-        if (file_exists($functionsFile)) {
-            include_once $functionsFile;
-        }
-
         // ===============================================
-
         // Enable FTP connector netmount
-        // elFinder::$netDrivers['ftp'] = 'FTP';
-
-        // ===============================================
-
-        // // Required for Dropbox network mount
-        // // Installation by composer
-        // // `composer require kunalvarma05/dropbox-php-sdk`
-        // // Enable network mount
-        // elFinder::$netDrivers['dropbox2'] = 'Dropbox2';
-        // // Dropbox2 Netmount driver need next two settings. You can get at https://www.dropbox.com/developers/apps
-        // // AND reuire regist redirect url to "YOUR_CONNECTOR_URL?cmd=netmount&protocol=dropbox2&host=1"
-        // define('ELFINDER_DROPBOX_APPKEY',    '');
-        // define('ELFINDER_DROPBOX_APPSECRET', '');
-        // ===============================================
-
-        // // Required for Google Drive network mount
-        // // Installation by composer
-        // // `composer require google/apiclient:^2.0`
-        // // Enable network mount
-        // elFinder::$netDrivers['googledrive'] = 'GoogleDrive';
-        // // GoogleDrive Netmount driver need next two settings. You can get at https://console.developers.google.com
-        // // AND reuire regist redirect url to "YOUR_CONNECTOR_URL?cmd=netmount&protocol=googledrive&host=1"
-        // define('ELFINDER_GOOGLEDRIVE_CLIENTID',     '');
-        // define('ELFINDER_GOOGLEDRIVE_CLIENTSECRET', '');
-        // // Required case of without composer
-        // define('ELFINDER_GOOGLEDRIVE_GOOGLEAPICLIENT', '/path/to/google-api-php-client/vendor/autoload.php');
-        // ===============================================
-
-        // // Required for Google Drive network mount with Flysystem
-        // // Installation by composer
-        // // `composer require nao-pon/flysystem-google-drive:~1.1 nao-pon/elfinder-flysystem-driver-ext`
-        // // Enable network mount
-        // elFinder::$netDrivers['googledrive'] = 'FlysystemGoogleDriveNetmount';
-        // // GoogleDrive Netmount driver need next two settings. You can get at https://console.developers.google.com
-        // // AND reuire regist redirect url to "YOUR_CONNECTOR_URL?cmd=netmount&protocol=googledrive&host=1"
-        // define('ELFINDER_GOOGLEDRIVE_CLIENTID',     '');
-        // define('ELFINDER_GOOGLEDRIVE_CLIENTSECRET', '');
-        // ===============================================
-
-        // // Required for One Drive network mount
-        // //  * cURL PHP extension required
-        // //  * HTTP server PATH_INFO supports required
-        // // Enable network mount
-        // elFinder::$netDrivers['onedrive'] = 'OneDrive';
-        // // GoogleDrive Netmount driver need next two settings. You can get at https://dev.onedrive.com
-        // // AND reuire regist redirect url to "YOUR_CONNECTOR_URL/netmount/onedrive/1"
-        // define('ELFINDER_ONEDRIVE_CLIENTID',     '');
-        // define('ELFINDER_ONEDRIVE_CLIENTSECRET', '');
-        // ===============================================
-
-        // // Required for Box network mount
-        // //  * cURL PHP extension required
-        // // Enable network mount
-        // elFinder::$netDrivers['box'] = 'Box';
-        // // Box Netmount driver need next two settings. You can get at https://developer.box.com
-        // // AND reuire regist redirect url to "YOUR_CONNECTOR_URL"
-        // define('ELFINDER_BOX_CLIENTID',     '');
-        // define('ELFINDER_BOX_CLIENTSECRET', '');
+        elFinder::$netDrivers['ftp'] = 'FTP';
         // ===============================================
 
         // Create directories
@@ -180,38 +104,57 @@ class CloudController extends \Controller
 
         // Documentation for connector options:
         // https://github.com/Studio-42/elFinder/wiki/Connector-configuration-options
-        $opts = array(
+        /**
+         * Simple function to demonstrate how to control file access using "accessControl" callback.
+         * This method will disable accessing files/folders starting from '.' (dot)
+         *
+         * @param  string    $attr    attribute name (read|write|locked|hidden)
+         * @param  string    $path    absolute file path
+         * @param  string    $data    value of volume option `accessControlData`
+         * @param  object    $volume  elFinder volume driver object
+         * @param  bool|null $isDir   path is directory (true: directory, false: file, null: unknown)
+         * @param  string    $relpath file path relative to volume root directory started with directory separator
+         * @return bool|null
+         **/
+        $accessFunction = function ($attr, $path, $data, $volume, $isDir, $relpath) {
+            $basename = basename($path);
+            return $basename[0] === '.'                  // if file/folder begins with '.' (dot)
+                   && strlen($relpath) !== 1           // but with out volume root
+                ? !($attr == 'read' || $attr == 'write') // set read+write to false, other (locked+hidden) set to true
+                :  null;                                 // else elFinder decide it itself
+        };
+        $opts = [
             // 'debug' => true,
-            'roots' => array(
+            'roots' => [
 
                 // Items volume
-                array(
+                [
                     'alias'         => 'Home',
                     'driver'        => 'LocalFileSystem',           // driver for accessing file system (REQUIRED)
                     'path'          => realpath($kernel->getRootDir().'/data/cloud/storage/'.$currentUser->getId()), // path to files (REQUIRED)
                     'URL'           => '/files/',                   // URL to files (REQUIRED)
                     'trashHash'     => 't1_Lw',                     // elFinder's hash of trash folder
                     'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
-                    'uploadDeny'    => array('all'),                // All Mimetypes not allowed to upload
-                    'uploadAllow'   => array('image', 'text/plain'),// Mimetype `image` and `text/plain` allowed to upload
-                    'uploadOrder'   => array('deny', 'allow'),      // allowed Mimetype `image` and `text/plain` only
-                    'accessControl' => 'access'                     // disable and hide dot starting files (OPTIONAL)
-                ),
+                    'uploadDeny'    => ['all'],                     // All Mimetypes not allowed to upload
+                    'uploadAllow'   => ['image', 'text/plain'],     // Mimetype `image` and `text/plain` allowed to upload
+                    'uploadOrder'   => ['deny', 'allow'],           // allowed Mimetype `image` and `text/plain` only
+                    'accessControl' => $accessFunction,             // disable and hide dot starting files (OPTIONAL)
+                ],
 
                 // Trash volume
-                array(
+                [
                     'id'            => '1',
                     'driver'        => 'Trash',
                     'path'          => realpath($kernel->getRootDir().'/data/cloud/storage/'.$currentUser->getId().'/.trash'),
                     'tmbURL'        => '../data/cloud/storage/'.$currentUser->getId().'/.trash/.tmb',
                     'winHashFix'    => DIRECTORY_SEPARATOR !== '/', // to make hash same to Linux one on windows too
-                    'uploadDeny'    => array('all'),                // Recommend the same settings as the original volume that uses the trash
-                    'uploadAllow'   => array('image', 'text/plain'),// Same as above
-                    'uploadOrder'   => array('deny', 'allow'),      // Same as above
-                    'accessControl' => 'access',                    // Same as above
-                )
-            )
-        );
+                    'uploadDeny'    => ['all'],                     // Recommend the same settings as the original volume that uses the trash
+                    'uploadAllow'   => ['image', 'text/plain'],     // Same as above
+                    'uploadOrder'   => ['deny', 'allow'],           // Same as above
+                    'accessControl' => $accessFunction,             // Same as above
+                ]
+            ]
+        ];
 
         // Run elFinder
         $connector = new elFinderConnector(new elFinder($opts));
